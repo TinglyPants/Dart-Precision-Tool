7f3a1265963a435be2f5e8c756326764ad1a7381
TinglyPants <chalkytangfastics@gmail.com>
2024-04-08 16:44:59 +0100

    Generates a greyscale image of points. Cool!

--- index.js
+++ index.js
@@ -43,6 +43,53 @@ const PlacePixel = (context, x, y, red, green, blue) => {
     context.fillRect(x, y, 1, 1);
 };
 
+const GetScoreFromArgument = (argument) => {
+    // Going anticlockwise from 0 to 60 degrees. This is all hard coded unfortunately.
+    if (0 <= argument && argument < 9) {
+        return 6;
+    } else if (9 <= argument && argument < 27) {
+        return 13;
+    } else if (27 <= argument && argument < 45) {
+        return 4;
+    } else if (45 <= argument && argument < 63) {
+        return 18;
+    } else if (63 <= argument && argument < 81) {
+        return 1;
+    } else if (81 <= argument && argument < 99) {
+        return 20;
+    } else if (99 <= argument && argument < 117) {
+        return 5;
+    } else if (117 <= argument && argument < 135) {
+        return 12;
+    } else if (135 <= argument && argument < 153) {
+        return 9;
+    } else if (153 <= argument && argument < 171) {
+        return 14;
+    } else if (171 <= argument && argument < 189) {
+        return 11;
+    } else if (189 <= argument && argument < 207) {
+        return 8;
+    } else if (207 <= argument && argument < 225) {
+        return 16;
+    } else if (225 <= argument && argument < 243) {
+        return 7;
+    } else if (243 <= argument && argument < 261) {
+        return 19;
+    } else if (261 <= argument && argument < 279) {
+        return 3;
+    } else if (279 <= argument && argument < 297) {
+        return 17;
+    } else if (297 <= argument && argument < 315) {
+        return 2;
+    } else if (315 <= argument && argument < 333) {
+        return 15;
+    } else if (333 <= argument && argument < 351) {
+        return 10;
+    } else if (351 <= argument && argument <= 360) {
+        return 6;
+    }
+};
+
 const boardSize = 500; // This value must be even. This is to ensure good argument calculations, as I am lazy.
 const boardCenterX = boardSize / 2 - 0.5; // Adjusted for 0 indexing.
 const boardCenterY = boardSize / 2 - 0.5; // Adjusted for 0 indexing.
@@ -64,21 +111,23 @@ for (let x = 0; x < boardSize; x++) {
         let distance = t.modulus;
         let angle = t.argument;
 
+        let value = GetScoreFromArgument(angle) * 4.25;
+
         if (distance < innerBullUpperBound) {
             //Point on inner bullseye
-            PlacePixel(ctx, x, y, 255, 0, 0);
+            PlacePixel(ctx, x, y, 50, 50, 50);
         } else if (distance < outerBullUpperBound) {
             //Point on outer bullseye
-            PlacePixel(ctx, x, y, 0, 255, 0);
+            PlacePixel(ctx, x, y, 25, 25, 25);
         } else if (trebleLowerBound < distance && distance < trebleUpperBound) {
             //Point on treble band
-            PlacePixel(ctx, x, y, 128, 128, 0);
+            PlacePixel(ctx, x, y, value * 3, value * 3, value * 3);
         } else if (doubleLowerBound < distance && distance < doubleUpperBound) {
             //Point on double band
-            PlacePixel(ctx, x, y, 0, 128, 128);
+            PlacePixel(ctx, x, y, value * 2, value * 2, value * 2);
         } else if (distance < doubleUpperBound) {
             // Point is on the board at least.
-            PlacePixel(ctx, x, y, angle * 0.70833333333, 0, 0);
+            PlacePixel(ctx, x, y, value, value, value);
         }
     }
 }
